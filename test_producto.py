4c33c9777994302947fdfff79740f7abd9314f85
Victor Gonzalo Tejada Delgado <vtejadad@ext.unicaja.es>
2025-08-17 13:26:13 +0200

    update file with black test_producto.py

--- test_producto.py
+++ test_producto.py
@@ -3,7 +3,6 @@ from producto import producto
 
 
 class TestProducto(unittest.TestCase):
-
     def test_producto(self):
         self.assertEqual(producto(3, 4), 12)
         self.assertEqual(producto(9, 0), 0)
